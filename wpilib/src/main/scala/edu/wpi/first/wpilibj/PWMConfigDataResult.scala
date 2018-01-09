38251834833e7a98433f17f59919551454d6077b
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-01-08 16:05:05 -0800

    Fix header comment location in PWMConfigDataResult

--- wpilib/src/main/scala/edu/wpi/first/wpilibj/PWMConfigDataResult.scala
+++ wpilib/src/main/scala/edu/wpi/first/wpilibj/PWMConfigDataResult.scala
@@ -1,11 +1,11 @@
-package edu.wpi.first.wpilibj
-
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2008-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj
 
 /**
   * Structure for holding the config data result for PWM.
