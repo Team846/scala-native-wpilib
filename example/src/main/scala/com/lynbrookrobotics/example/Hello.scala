76fc7c1450dbdfe89c23001e1fd59c69496aceae
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-01-18 20:20:35 -0800

    More complete example with fast loop

--- example/src/main/scala/com/lynbrookrobotics/example/Hello.scala
+++ example/src/main/scala/com/lynbrookrobotics/example/Hello.scala
@@ -1,11 +1,17 @@
 package com.lynbrookrobotics.example
 
-import edu.wpi.first.wpilibj.IterativeRobot
-import edu.wpi.first.wpilibj.hal.HAL
-
-import scala.scalanative.native._
+import edu.wpi.first.wpilibj.{AnalogInput, IterativeRobot, Notifier, Servo}
 
 object Hello extends IterativeRobot {
+  lazy val in = new AnalogInput(0)
+  lazy val out = new Servo(0)
+
+  val notifier = new Notifier(new Runnable {
+    override def run(): Unit = out.set(in.getAverageVoltage / 5)
+  })
+
+  notifier.startPeriodic(0.005)
+
   override def main(args: Array[String]): Unit = {
     super.main(args)
   }
