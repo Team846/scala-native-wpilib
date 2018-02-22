6b461f9526e93a04eb0cd18abfe54430ebe83ea3
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-02-22 13:41:16 -0800

    Update hardcoded WPILib version

--- wpilib/src/main/scala/edu/wpi/first/wpilibj/util/WPILibVersion.scala
+++ wpilib/src/main/scala/edu/wpi/first/wpilibj/util/WPILibVersion.scala
@@ -2,5 +2,5 @@ package edu.wpi.first.wpilibj.util
 
 object WPILibVersion {
   // Shadaj: "should we gen this like WPILib or is that too much work?"
-  val Version = "2018.1.1"
+  val Version = "2018.3.1"
 }
